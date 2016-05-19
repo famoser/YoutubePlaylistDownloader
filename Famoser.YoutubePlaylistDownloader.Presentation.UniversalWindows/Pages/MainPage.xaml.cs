eb5c0daea5cf122a58753d2f8bc2f481e465d9a4
famoser <git@famoser.ch>
2016-05-19 09:18:07 +0200

    navigation changes

--- Famoser.YoutubePlaylistDownloader.Presentation.UniversalWindows/Pages/MainPage.xaml.cs
+++ Famoser.YoutubePlaylistDownloader.Presentation.UniversalWindows/Pages/MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Windows.UI.Xaml.Controls;
+using Famoser.YoutubePlaylistDownloader.Business.Models;
+using Famoser.YoutubePlaylistDownloader.View.ViewModels;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -12,6 +14,16 @@ namespace Famoser.YoutubePlaylistDownloader.Presentation.UniversalWindows
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        public MainPageViewModel MainPageViewModel { get { return DataContext as MainPageViewModel; } }
+
+        private void ListViewBase_OnItemClick(object sender, ItemClickEventArgs e)
+        {
+            var playlist = e.ClickedItem as PlaylistModel;
+            if (playlist != null)
+                if (MainPageViewModel.SelectPlaylistCommand.CanExecute(playlist))
+                    MainPageViewModel.SelectPlaylistCommand.Execute(playlist);
         }
     }
 }
